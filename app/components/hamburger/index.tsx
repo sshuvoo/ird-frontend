2eeba96b54f306da68105223a01bee2be30a642d
sshuvoo <safayanshuvo@gmail.com>
2024-11-22 23:59:48 +0600

    Fix vercel warning - 1

--- app/components/hamburger/index.tsx
+++ app/components/hamburger/index.tsx
@@ -28,7 +28,7 @@ export function HamburgerMenu({ children }: { children: React.ReactNode }) {
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             ></path>
           </svg>
-          Dua's importance
+          Menu
         </button>
       </div>
       {isOpen &&
